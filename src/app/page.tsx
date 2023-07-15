5ca705b17675c969eb6e61e9d177e4dfa071b7db
Gabriel Tiburcio <gabrieltiburcio0811@gmail.com>
2023-07-15 17:13:09 -0300

    feature: fiz a footer e a main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="h-screen flex flex-col">
 
-      <div className="flex flex-1">
+      <div className="flex flex-1 "> 
         <aside className="w-72 bg-zinc-950 p-6">
           <div className='flex items-center gap-2'>
             <div className='w-3 h-3 bg-red-500 rounded-full' />
